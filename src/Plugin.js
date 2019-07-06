017eeff8fe95ccb2fbf1b2cdc4e44a2304a4cf03
Floofies <fantasticdragons@gmail.com>
2019-07-06 15:40:39 -0400

    Added BlockStatement support

--- src/Plugin.js
+++ src/Plugin.js
@@ -2,23 +2,41 @@ function pluginFactory(options = []) {
 	const visitor = {
 		"LabeledStatement": {
 			exit: function (path) {
+				if (path.node.label.name !== "argutia") return;
 				const labelBody = path.node.body;
-				if (
-					path.node.label.name !== "argutia"
-					|| labelBody.type !== "ExpressionStatement"
-					|| labelBody.expression.type !== "StringLiteral"
-				) return;
-				if (options.includes(labelBody.expression.value)) {
-					path.remove();
-					path.skip();
-				} else {
-					var parentPath = path.getFunctionParent();
-					if (parentPath === null) parentPath = path.scope.getProgramParent();
-					if (path.container.length >= path.key) {
-						path.container.splice(path.key + 1, 1);
+				if (labelBody.type === "StringLiteral") {
+					var labelStr = labelBody.expression.value;
+				} else if (labelBody.type === "BlockStatement") {
+					var blockBody = labelBody.body;
+					if (
+						blockBody.length === 0
+						|| blockBody[0].type !== "ExpressionStatement"
+						|| blockBody[0].expression.type !== "StringLiteral"
+					) return;
+					var labelStr = blockBody[0].expression.value;
+				} else return;
+				if (options.includes(labelStr)) {
+					if (labelBody.type === "BlockStatement") {
+						blockBody.shift();
+						path.container.splice(path.key, 1, ...blockBody);
+						path.skip();
+					} else {
+						path.remove();
+						path.skip();
 					}
-					path.remove();
-					path.skip();
+				} else {
+					if (labelBody.type === "BlockStatement") {
+						path.remove();
+						path.skip();
+					} else {
+						var parentPath = path.getFunctionParent();
+						if (parentPath === null) parentPath = path.scope.getProgramParent();
+						if (path.container.length >= path.key) {
+							path.container.splice(path.key + 1, 1);
+						}
+						path.remove();
+						path.skip();
+					}
 				}
 			}
 		}
